c83795efdd4eb5aeeb8aeb6781740d7c3080d875
mechztcc <dojathan@gmail.com>
2023-12-08 08:38:06 -0500

    feat: added google font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Navbar() {
   return (
     <div className="flex w-full bg-zinc-200 h-20 justify-between items-center p-3 shadow-lg">
-      <h1 className="font-bold text-3xl">MOVIES</h1>
+      <h1 className="font-bold text-3xl title text-red-400 cursor-pointer hover:text-red-500">MOVIES</h1>
 
       <div className="relative">
         <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute right-2 bottom-2 text-red-400 bg-red-200 p-2 rounded-full hover:text-white hover:bg-red-400 cursor-pointer"/>
