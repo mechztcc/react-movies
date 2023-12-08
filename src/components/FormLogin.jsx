1a5fda90b51f60badf1430d093393cbef0121e1c
mechztcc <dojathan@gmail.com>
2023-12-08 11:51:11 -0500

    feat: simple using zod to validate form

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,32 +1,64 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 export default function FormLogin() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    navigate('/')
+  };
+
   return (
-    <div className="flex flex-col h-screen justify-center mx-10">
-      <h1 className="text-6xl text-center ">Login Account</h1>
-      <span className="mb-3 text-center">Fill form to access your account</span>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col h-screen justify-center mx-10">
+        <h1 className="text-6xl text-center ">Login Account</h1>
+        <span className="mb-3 text-center">
+          Fill form to access your account
+        </span>
 
-      <label className="mt-3 font-semibold" htmlFor="Email">
-        E-mail
-      </label>
-      <input
-        type="text"
-        className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100"
-      />
+        <label className="mt-3 font-semibold" htmlFor="Email">
+          E-mail
+        </label>
+        <input
+          {...register("email", { required: "E-mail is required." })}
+          type="text"
+          className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100"
+        />
 
-      <label className="mt-3 font-semibold" htmlFor="Email">
-        Password
-      </label>
-      <input
-        type="text"
-        className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100"
-      />
+        <small className="text-red-400 mt-2">{errors.email?.message}</small>
 
-      <button className="mt-5 bg-red-400 p-3 rounded-full text-white font-semibold shadow-lg hover:bg-red-500">
-        SUBMIT
-      </button>
+        <label className="mt-3 font-semibold" htmlFor="Email">
+          Password
+        </label>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: 6,
+          })}
+          type="text"
+          className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100"
+        />
+        <small className="text-red-400 mt-2">{errors.password?.message}</small>
+        <button
+          type="submit"
+          className="mt-5 bg-red-400 p-3 rounded-full text-white font-semibold shadow-lg hover:bg-red-500"
+        >
+          SUBMIT
+        </button>
 
-      <span className="text-center mt-10">
-        Don't have account? <b className="text-red-400 cursor-pointer hover:text-red-500">Create Account</b>
-      </span>
-    </div>
+        <span className="text-center mt-10">
+          Don't have account?{" "}
+          <b className="text-red-400 cursor-pointer hover:text-red-500">
+            Create Account
+          </b>
+        </span>
+      </div>
+    </form>
   );
 }
