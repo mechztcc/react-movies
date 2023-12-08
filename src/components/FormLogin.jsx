4d7c3895a074346f96c7d4ee59142bac995178aa
mechztcc <dojathan@gmail.com>
2023-12-08 13:53:06 -0500

    feat: hide / show password

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { faE, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FormLogin() {
   const navigate = useNavigate();
+
+  const [isPass, setIsPass] = useState(false);
 
   const {
     register,
@@ -11,7 +16,7 @@ export default function FormLogin() {
   } = useForm();
 
   const onSubmit = (data) => {
-    navigate('/')
+    navigate("/");
   };
 
   return (
@@ -36,25 +41,40 @@ export default function FormLogin() {
         <label className="mt-3 font-semibold" htmlFor="Email">
           Password
         </label>
-        <input
-          {...register("password", {
-            required: "Password is required",
-            minLength: 6,
-          })}
-          type="text"
-          className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100"
-        />
+
+        <div className="relative flex justify-end items-center">
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: 6,
+            })}
+            type={isPass ? "text" : "password"}
+            className="rounded-full border p-3 px-5 mt-2 border-zinc-400 focus:outline-red-400 focus:bg-red-100 w-full"
+          />
+          <button
+             onClick={() => {
+             setIsPass(!isPass)
+            }}
+            type="button"
+            className="rounded-full absolute top-5 right-3 px-2 py-1 bg-red-200 flex justify-center items-center"
+          >
+            <FontAwesomeIcon
+              icon={isPass ? faEye : faEyeSlash}
+              className="text-red-400"
+            />
+          </button>
+        </div>
         <small className="text-red-400 mt-2">{errors.password?.message}</small>
         <button
           type="submit"
-          className="mt-5 bg-red-400 p-3 rounded-full text-white font-semibold shadow-lg hover:bg-red-500"
+          className="mt-5 bg-red-400 p-3 rounded-full text-white font-semibold shadow-lg hover:bg-red-500 hover:shadow-red-300"
         >
           SUBMIT
         </button>
 
         <span className="text-center mt-10">
           Don't have account?{" "}
-          <b className="text-red-400 cursor-pointer hover:text-red-500">
+          <b className="text-red-400 cursor-pointer hover:text-red-500 hover:shadow-red-300">
             Create Account
           </b>
         </span>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
       <div className="col-span-1 md:col-span-1">
         <FormLogin />
       </div>
-      <div className="col-span-1 md:col-span-2 bg-red-500">
+      <div className="col-span-1 md:col-span-2 bg-red-500 ">
         <img
           className="w-full h-full object-cover"
           src="https://img.freepik.com/free-photo/view-3d-cinema-elements_23-2150720802.jpg?t=st=1702052487~exp=1702056087~hmac=13eb80cb37896a56698eb0cb973287f36d6996f1fbef79029a0868c57e259739&w=1380"
