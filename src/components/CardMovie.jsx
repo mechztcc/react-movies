75d418a92dc0f67ccf887b99901acf1121ee01ed
mechztcc <dojathan@gmail.com>
2023-12-08 09:07:11 -0500

    fix: improviments on home page

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,43 +1,37 @@
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
 import { add } from "../stores/reducer";
 
-export default function CardMovie({props}) {
+export default function CardMovie({ props }) {
   const dispatch = useDispatch();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 my-3 mx-3">
       <div className="col-span-1 items-center flex justify-center">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg"
-          alt=""
-          className="rounded-3xl w-1/2"
-        />
+        <img src={props.image} alt="" className="rounded-3xl w-1/2" />
       </div>
       <div className="col-span-1 flex flex-col justify-between">
         <div className="mt-10">
           <h1 className="font-semibold">{props.name}</h1>
-          <span className="text-zinc-500 mt-3">
-            { props.description }
-          </span>
+          <span className="text-zinc-500 mt-3">{props.description}</span>
 
           <div className="flex mt-3">
             <p className="bg-blue-600 rounded-full text-center text-white font-semibold px-3">
-              { props.category }
+              {props.category}
             </p>
             <p className="bg-blue-600 rounded-full text-center text-white font-semibold mx-1 px-3">
-              { props.duration }
+              {props.duration}
             </p>
           </div>
         </div>
 
         <button
           className="mt-5 bg-red-400 p-3 rounded-full text-white font-semibold shadow-lg hover:bg-red-500"
-          onClick={() => dispatch(add({ id: 11 }))}
+          onClick={() => dispatch(add(props))}
         >
-          <FontAwesomeIcon icon={faPlay} className="mx-2" />
-          <span>ASSISTIR</span>
+          <FontAwesomeIcon icon={faHeart} className="mx-2" />
+          <span>FAVORITOS</span>
         </button>
       </div>
     </div>
--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -13,7 +13,7 @@ export const homeSlice = createSlice({
         score: "9.3",
         totalScore: "2.8mi",
         duration: "2h 22 min",
-        image: "assets/um-sonho-de-liberdade.jpeg",
+        image: "https://br.web.img2.acsta.net/medias/nmedia/18/90/16/48/20083748.jpg",
         embed: "https://www.youtube.com/embed/EIc_wJtm6AU?si=HaXeyRelkd9lDHxX",
         description:
           "Two imprisoned men reunite over the course of several years, finding solace and eventual redemption through acts of common decency.",
@@ -27,7 +27,7 @@ export const homeSlice = createSlice({
         score: "9.2",
         totalScore: "2.3mi",
         duration: "2h 55 min",
-        image: "assets/poderoso-chefao.jpeg",
+        image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
         embed: "https://www.youtube.com/embed/SaHZHU-44XA?si=e7CIpa3vVmKIRcSI",
         description:
           "The aging patriarch of an organized crime dynasty passes control of his clandestine empire to his reluctant son.",
@@ -41,7 +41,7 @@ export const homeSlice = createSlice({
         score: "9.0",
         totalScore: "2.8mi",
         duration: "2h 32 min",
-        image: "assets/batman.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
         embed: "https://www.youtube.com/embed/EXeTwQWrcwY?si=PKH7SB-VAXCyt0Ag",
         description:
           "Now with the help of Lieutenant Jim Gordon and District Attorney Harvey Dent, Batman has what it takes to banish crime from Gotham City once and for all. But soon, the three will fall victim to the Joker, who intends to throw Gotham into anarchy.",
@@ -55,7 +55,7 @@ export const homeSlice = createSlice({
         score: "8.9",
         totalScore: "2.2mi",
         duration: "2h 34 min",
-        image: "assets/pulp-fiction.jpg",
+        image: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15684_p_v13_an.jpg",
         embed: "https://www.youtube.com/embed/s7EdQ4FqbhY?si=csb96tMftEonCEN-",
         description:
           "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of thugs intertwine in four stories of violence and redemption.",
@@ -69,7 +69,7 @@ export const homeSlice = createSlice({
         score: "8.8",
         totalScore: "2.3mi",
         duration: "2h 19 min",
-        image: "assets/clube-luta.jpg",
+        image: "https://m.media-amazon.com/images/I/81JWVTlPQ2L._AC_UF894,1000_QL80_.jpg",
         embed: "https://www.youtube.com/embed/O1nDozs-LxI?si=BGhq5hqePlMWGkT_",
         description:
           "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of thugs intertwine in four stories of violence and redemption.",
@@ -83,7 +83,7 @@ export const homeSlice = createSlice({
         score: "8.8",
         totalScore: "2.3mi",
         duration: "2h 2 min",
-        image: "assets/coringa.webp",
+        image: "https://assets.vogue.in/photos/5d7224d50ce95e0008696c55/2:3/w_2560%2Cc_limit/Joker.jpg",
         embed: "https://www.youtube.com/embed/zAGVQLHvwOY?si=hlWZRoXMsT73MEoN",
         description:
           "A failed comedian goes crazy and becomes a psychopathic killer.",
@@ -92,7 +92,7 @@ export const homeSlice = createSlice({
   },
   reducers: {
     add: (state, action) => {
-      console.log(state.movies[0].name);
+      console.log(action.payload);
     },
   },
 });
